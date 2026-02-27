2cc99b6735da88d9e3d502888f399d1395fba242
Érick <72041638+ErickSolon@users.noreply.github.com>
2026-02-26 23:21:01 -0300

    Update Program.cs

--- backend/FortalRPAPI/FortalRPAPI/Program.cs
+++ backend/FortalRPAPI/FortalRPAPI/Program.cs
@@ -14,11 +14,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // 🔹 CONFIGURAR PIPELINE AQUI
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
@@ -26,5 +26,38 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Administradores}/{action=Index}/{id?}");
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
+
+var allowedIps = new List<string>
+{
+    "", // COLOQUE SEU IP MANUALMENTE AQUI PARA ACESSAR A PAGINA DE ADMINISTRADORES
+    "127.0.0.1",
+    "::1"};
+
+app.Use(async (context, next) =>
+{
+    var path = context.Request.Path.Value?.ToLower();
+
+    // Bloqueia index e tudo relacionado a Administradores
+    if (path == "/" || path.StartsWith("/administradores"))
+    {
+        var remoteIp = context.Request.Headers["X-Forwarded-For"].FirstOrDefault()
+                       ?? context.Connection.RemoteIpAddress?.ToString();
+
+        if (!allowedIps.Contains(remoteIp))
+        {
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsync("Acesso negado.");
+            return;
+        }
+    }
+
+    await next();
+});
 
 app.Run();
